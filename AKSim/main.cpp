8a6a4dce0cb60da4e2de88db5aa4c807b486bf9c
Cícero Camargo <camargo.cicero@gmail.com>
2012-08-09 00:24:14 -0300

    Initial Commit

---
+++ AKSim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AKSim
+//
+//  Created by Cícero Camargo on 09/08/12.
+//  Copyright (c) 2012 Cícero Camargo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
